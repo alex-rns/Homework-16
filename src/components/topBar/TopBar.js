16ae5c6f628e04acd392c57da504862f73ad7fba
alex-rns <alexrns3@gmail.com>
2018-02-14 21:50:54 +0200

    styling topbar

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import './TopBar.css'
+import { Navbar, Header, Button, Glyphicon, Nav, NavItem, NavDropdown, MenuItem, Image } from 'react-bootstrap/lib'
+import userPhoto from '../../assets/img/user-photo.png'
+
 
 
 class TopBar extends React.Component {
   render() {
     return(
-      <div>
-        <h1>top bar</h1>
-      </div>
+      <Navbar>
+
+        <Navbar.Header>
+          <Button>
+            <Glyphicon glyph="align-justify" />
+          </Button>
+          <Button>
+            <Glyphicon glyph="search" />
+          </Button>
+        </Navbar.Header>
+
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            <Image src={userPhoto} circle />
+          </NavItem>
+          <NavDropdown eventKey={3} id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Log out</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
     )
   }
 }
