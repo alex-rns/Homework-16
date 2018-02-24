503c8a8834d475d19c4df8cc4afb83a349e7ed2c
alexrns <alexrns3@gmail.com>
2018-02-24 15:21:33 +0200

    report chart styling

--- src/config/reportChart.config.js
+++ src/config/reportChart.config.js
@@ -1,36 +1,52 @@
 const ReportChart = {
-  chart: {
-    type: 'spline',
-    height: '250'
-  },
-  title: {
-    text: ''
-  },
-  xAxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    gridLineWidth: 1,
-    labels: {
-      enabled: false
+    chart: {
+        type: 'spline',
+        height: '250'
     },
-  },
-  yAxis: {
-    description: null,
-    title: '',
-  },
-  series: [{
-    data: []
-  }],
-  credits: {
-    enabled: false
-  },
-  legend: {
-    enabled: false
-  },
-  tooltip: {
-    shared: true,
-    crosshairs: true
-  },
+    title: {
+        text: null
+    },
+    xAxis: {
+        tickWidth: 0,
+        gridLineWidth: 1,
+        labels: {
+            enabled: false
+        },
+    },
+    yAxis: {
+        tickWidth: 0,
+        description: null,
+        title: '',
+        max: 700,
+        min: 100,
+        tickInterval: 100,
+
+    },
+    plotOptions: {
+        spline: {
+            lineWidth: 6,
+            marker: {
+                enabled: false
+            },
+        },
+        series: {
+            color: '#5584ff'
+        }
+    },
+
+    series: [{
+        data: []
+    }],
+    credits: {
+        enabled: false
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        shared: true,
+        crosshairs: true
+    },
 };
 
 export default ReportChart;
