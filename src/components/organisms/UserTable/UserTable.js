7830c437b276e4e7d2cab0ed3cd8fdda908fde30
alex-rns <alexrns3@gmail.com>
2018-03-08 21:56:13 +0200

    add users filter!

--- src/components/organisms/UserTable/UserTable.js
+++ src/components/organisms/UserTable/UserTable.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 import './UserTable.css'
 import onlineIndicator from '../../../assets/img/online-indicator.png'
 import FontAwesome from 'react-fontawesome';
 
 
-
 class UserTable extends React.Component {
 
   userNameBlock = (cell, row) => {
     return (
-      <div className="user-name-block" style={{display:'flex', alignItems:'center'}}>
+      <div className="user-name-block" style={{display: 'flex', alignItems: 'center'}}>
         <div className="user-image">
           <img className="user-photo" src={row.userPhoto} alt={row.userName}/>
           {
@@ -28,22 +27,21 @@ class UserTable extends React.Component {
   };
 
 
-
-  lastActivityBlock =(cell, row) => {
-    return(
+  lastActivityBlock = (cell, row) => {
+    return (
       <div className="last-activity-block">
-        <p className = {row.active ? 'online-now': ''}>
+        <p className={row.active ? 'online-now' : ''}>
           <FontAwesome
-          className='task-time-icon'
-          name='clock'
-        />
+            className='task-time-icon'
+            name='clock'
+          />
           {row.lastActivity}</p>
       </div>
     )
   };
 
-  emailBlock =(cell, row) => {
-    return(
+  emailBlock = (cell, row) => {
+    return (
       <div className="mail-and-phone-block">
         <p>
           {row.email}</p>
@@ -51,8 +49,8 @@ class UserTable extends React.Component {
     )
   };
 
-  phoneBlock =(cell, row) => {
-    return(
+  phoneBlock = (cell, row) => {
+    return (
       <div className="mail-and-phone-block">
         <p>
           {row.phone}</p>
@@ -60,15 +58,15 @@ class UserTable extends React.Component {
     )
   };
 
-  buttonBlock =() => {
-    return(
-      <div className="button-block">
+  buttonBlock = () => {
+    return (
+      <button className="button-block">
         <FontAwesome
           className='dropdown-button-icon'
           name='ellipsis-v'
           size='2x'
         />
-      </div>
+      </button>
     )
   };
 
@@ -79,36 +77,46 @@ class UserTable extends React.Component {
     return (
       <div>
         <BootstrapTable
-          headerStyle={ { background: '#f6f7fa', font: '400 1.2vmax Roboto' }}
-          containerStyle={ { background: '#fff' }}
+          bordered={false}
+          headerStyle={{background: '#f6f7fa', font: '400 1.2vmax Roboto', paddingLeft: 20}}
+          containerStyle={{background: '#fff'}}
           data={this.props.data}
           pagination>
 
           <TableHeaderColumn
-
+            thStyle={{'border': 'none'}}
+            width='30%'
             dataField='userName'
             isKey
             dataFormat={this.userNameBlock}
           >Name</TableHeaderColumn>
 
           <TableHeaderColumn
+
+            thStyle={{'border': 'none'}}
+            width='20%'
             dataField='lastActivity'
             dataFormat={this.lastActivityBlock}
           >Last activity</TableHeaderColumn>
 
 
           <TableHeaderColumn
+            thStyle={{'border': 'none'}}
+            width='20%'
             dataField='email'
             dataFormat={this.emailBlock}
           >Mail</TableHeaderColumn>
 
 
           <TableHeaderColumn
+            thStyle={{'border': 'none'}}
+            width='20%'
             dataField='phone'
             dataFormat={this.phoneBlock}
           >Phone</TableHeaderColumn>
 
           <TableHeaderColumn
+            thStyle={{'border': 'none'}}
             width='10%'
             dataField='button'
             dataFormat={this.buttonBlock}
--- src/components/pages/users/Users.js
+++ src/components/pages/users/Users.js
@@ -3,7 +3,7 @@ import {Grid, Row} from 'react-bootstrap/lib';
 import './Users.css'
 
 //component
-
+import Select from "../../atoms/Select/Select";
 import UserTable from "../../organisms/UserTable/UserTable";
 
 import JohnDoe from '../../../assets/img/John Doe.png'
@@ -15,8 +15,10 @@ import PatricSmith from '../../../assets/img/Patric Smith.png'
 import NinaJones from '../../../assets/img/Nina Jones.png'
 
 
-
-
+const usersSelect = {
+  list: ["Active First", "No Active", "All Users"],
+  label: 'Sort'
+};
 
 
 const users = [
@@ -64,22 +66,409 @@ const users = [
     email: 'patricksmith@design.com',
     phone: '(000) 111 222 333',
     active: false
-  }
-];
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
 
+  {
+    userPhoto: JohnDoe,
+    userName: 'John Doe',
+    position: 'CEO',
+    lastActivity: 'Online now!',
+    email: 'johndoe@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: NinaJones,
+    userName: 'Nina Jones',
+    position: 'UX Designer',
+    lastActivity: 'Online now!',
+    email: 'ninajones@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AlexSmith,
+    userName: 'Alex Smith',
+    position: 'Web Designer',
+    lastActivity: 'Online now!',
+    email: 'alexsmith@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AnnClooney,
+    userName: 'Ann Clooney',
+    position: 'Account Manager',
+    lastActivity: '20 minutes ago',
+    email: 'annclooney@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: PatricSmith,
+    userName: 'Patric Smith',
+    position: 'Project Manager',
+    lastActivity: '40 minutes ago',
+    email: 'patricksmith@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JohnDoe,
+    userName: 'John Doe',
+    position: 'CEO',
+    lastActivity: 'Online now!',
+    email: 'johndoe@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: NinaJones,
+    userName: 'Nina Jones',
+    position: 'UX Designer',
+    lastActivity: 'Online now!',
+    email: 'ninajones@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AlexSmith,
+    userName: 'Alex Smith',
+    position: 'Web Designer',
+    lastActivity: 'Online now!',
+    email: 'alexsmith@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AnnClooney,
+    userName: 'Ann Clooney',
+    position: 'Account Manager',
+    lastActivity: '20 minutes ago',
+    email: 'annclooney@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: PatricSmith,
+    userName: 'Patric Smith',
+    position: 'Project Manager',
+    lastActivity: '40 minutes ago',
+    email: 'patricksmith@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JohnDoe,
+    userName: 'John Doe',
+    position: 'CEO',
+    lastActivity: 'Online now!',
+    email: 'johndoe@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: NinaJones,
+    userName: 'Nina Jones',
+    position: 'UX Designer',
+    lastActivity: 'Online now!',
+    email: 'ninajones@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AlexSmith,
+    userName: 'Alex Smith',
+    position: 'Web Designer',
+    lastActivity: 'Online now!',
+    email: 'alexsmith@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AnnClooney,
+    userName: 'Ann Clooney',
+    position: 'Account Manager',
+    lastActivity: '20 minutes ago',
+    email: 'annclooney@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: PatricSmith,
+    userName: 'Patric Smith',
+    position: 'Project Manager',
+    lastActivity: '40 minutes ago',
+    email: 'patricksmith@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+
+  {
+    userPhoto: JohnDoe,
+    userName: 'John Doe',
+    position: 'CEO',
+    lastActivity: 'Online now!',
+    email: 'johndoe@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: NinaJones,
+    userName: 'Nina Jones',
+    position: 'UX Designer',
+    lastActivity: 'Online now!',
+    email: 'ninajones@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AlexSmith,
+    userName: 'Alex Smith',
+    position: 'Web Designer',
+    lastActivity: 'Online now!',
+    email: 'alexsmith@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AnnClooney,
+    userName: 'Ann Clooney',
+    position: 'Account Manager',
+    lastActivity: '20 minutes ago',
+    email: 'annclooney@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: PatricSmith,
+    userName: 'Patric Smith',
+    position: 'Project Manager',
+    lastActivity: '40 minutes ago',
+    email: 'patricksmith@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JohnDoe,
+    userName: 'John Doe',
+    position: 'CEO',
+    lastActivity: 'Online now!',
+    email: 'johndoe@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: NinaJones,
+    userName: 'Nina Jones',
+    position: 'UX Designer',
+    lastActivity: 'Online now!',
+    email: 'ninajones@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AlexSmith,
+    userName: 'Alex Smith',
+    position: 'Web Designer',
+    lastActivity: 'Online now!',
+    email: 'alexsmith@design.com',
+    phone: '(000) 111 222 333',
+    active: true
+  },
+  {
+    userPhoto: AnnClooney,
+    userName: 'Ann Clooney',
+    position: 'Account Manager',
+    lastActivity: '20 minutes ago',
+    email: 'annclooney@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: PatricSmith,
+    userName: 'Patric Smith',
+    position: 'Project Manager',
+    lastActivity: '40 minutes ago',
+    email: 'patricksmith@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: NickyHunt,
+    userName: 'Nicky Hunt',
+    position: 'Product Designer',
+    lastActivity: '1 hour ago',
+    email: 'nickyhunt@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  },
+  {
+    userPhoto: JaneDoe,
+    userName: 'Jane Doe',
+    position: 'Graphic Designer',
+    lastActivity: '2 days ago',
+    email: 'janedoe@design.com',
+    phone: '(000) 111 222 333',
+    active: false
+  }
+
+];
 
 
 class Users extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: []
+    }
+  }
+
+  componentWillMount() {
+    let active = users.filter((user) => {
+        return user.active
+      }
+    );
+    this.setState({
+      activeUsers: active
+    })
+  }
+
+
+  onChange = (e) => {
+    if (e.target.value === "Active First") {
+      let active = users.filter((user) => {
+          return user.active
+        }
+      );
+      this.setState({
+        activeUsers: active
+      })
+
+    }
+    else if (e.target.value === "No Active") {
+      let active = users.filter((user) => {
+          return !user.active
+        }
+      );
+      this.setState({
+        activeUsers: active
+      });
+
+    }
+    else {
+      this.setState({
+        activeUsers: users
+      });
+    }
+
+
+  };
+
   render() {
+
 
     return (
       <div className="Users">
-        <h2>Users <span>({users.length})</span></h2>
+        <div className="users-header">
+          <h2>Users <span>({users.length})</span></h2>
+          <Select onChange={this.onChange} data={usersSelect}/>
+        </div>
 
         <Grid fluid>
           <Row>
-            <UserTable data={users}/>
+            <UserTable data={this.state.activeUsers}/>
           </Row>
         </Grid>
 
