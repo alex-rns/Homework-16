eb5a87b767c31ed184c80b91bce874337b2393d6
alex-rns <alexrns3@gmail.com>
2018-03-10 23:30:38 +0200

    + workflow dnd

--- src/components/pages/workflow/Workflow.js
+++ src/components/pages/workflow/Workflow.js
@@ -11,65 +11,64 @@ import WorkflowCompleted from "../../organisms/WorkflowCompleted/WorkflowComplet
 
 const dataWorkflowToDo = [
   {
-    title: 'New website for Symu.co',
-    deadline: '5 days delays',
-    deadlineCheck: true
-  },
-  {
-    title: 'Free business PSD Template',
-    deadline: '2 days delays',
-    deadlineCheck: true
-  },
-  {
-    title: 'New logo for JCD.pl',
+    title: 'New website for JCD.pl',
     deadline: '5 days left'
   },
   {
-    title: 'Free Icons Set vol. 3',
-    deadline: '10 days left'
+    title: 'Free PSD Template vol. 3',
+    deadline: '5 days left'
+  },
+  {
+    title: 'New logo for Google',
+    deadline: '5 days left'
+  },
+  {
+    title: 'New website for Windu.org',
+    deadline: '5 days left'
+  },
+  {
+    title: 'Free PSD Template vol. 2',
+    deadline: '5 days left'
+  },
+  {
+    title: 'Iconset Vol. 3',
+    deadline: '5 days left'
   },
 ];
 
 const dataWorkflowProgress = [
   {
-    title: 'New website for Symu.co',
-    deadline: '5 days delays',
-    deadlineCheck: true
+    title: 'New logo for Apple',
+    deadline: '1 days left'
   },
   {
-    title: 'Free business PSD Template',
-    deadline: '2 days delays',
-    deadlineCheck: true
-  },
-  {
-    title: 'New logo for JCD.pl',
+    title: 'Daily UI Kit',
     deadline: '5 days left'
   },
   {
-    title: 'Free Icons Set vol. 3',
-    deadline: '10 days left'
-  },
+    title: 'Business Cards',
+    deadline: '2 days delays',
+    deadlineCheck: true
+  }
 ];
 
 const dataWorkflowCompleted = [
   {
+    title: 'Free PSD Template vol. 1',
+    deadline: 'Completed!'
+  },
+  {
+    title: 'Iconset vol. 1',
+    deadline: 'Completed!'
+  },
+  {
     title: 'New website for Symu.co',
-    deadline: '5 days delays',
-    deadlineCheck: true
+    deadline: 'Completed!'
   },
   {
-    title: 'Free business PSD Template',
-    deadline: '2 days delays',
-    deadlineCheck: true
-  },
-  {
-    title: 'New logo for JCD.pl',
-    deadline: '5 days left'
-  },
-  {
-    title: 'Free Icons Set vol. 3',
-    deadline: '10 days left'
-  },
+    title: 'Iconset vol. 2',
+    deadline: 'Completed!'
+  }
 ];
 
 
